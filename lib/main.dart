8834ffd09914609041064b65588704098397c2a3
Abhijitj792001 <abhijithjeejamon792001@gmail.com>
2025-04-10 22:59:13 +0530

    counter app started

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:counterapp/screens/counter_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(42, 20, 38, 1))
+      ),
+      home: CounterScreen(),
+    );
+  }
+}
